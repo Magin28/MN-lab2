f24e579426624ab4ff00f24f4c202c6decf24f8d
Magin28 <marcin.ostachowicz@gmail.com>
2025-10-15 14:21:29 +0200

    Nie wiem czemu nie przechodzi jeden test

--- main.py
+++ main.py
@@ -84,36 +84,56 @@ def parallel_plot(x1:np.ndarray,y1:np.ndarray,x2:np.ndarray,y2:np.ndarray,
     matplotlib.pyplot.figure  
         Figura z dwoma wykresami (x1, y1) i (x2, y2), zgodna z opisem z zadania 5.  
     """
-    return None
-
+    
+    if x1.size == 0 or y1.size == 0 or x2.size == 0 or y2.size == 0:
+        return None
+    if  x2.size != y2.size or x1.size != y1.size:
+        return None
+    if len(np.unique(x1)) != x1.size or len(np.unique(x2)) != x2.size:
+        return None
+    if orientation == '-':
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+    elif orientation == '|':
+        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(5, 12))
+    else:
+        return None
+    
+    ax1.plot(x1, y1)
+    ax2.plot(x2, y2)
+    ax1.set(xlabel=x1label, ylabel=y1label)
+    ax2.set(xlabel=x2label, ylabel=y2label)
+    fig.suptitle(title)
+    plt.show()
+    return fig
 
 def log_plot(x:np.ndarray,y:np.ndarray,xlabel:np.ndarray,ylabel:str,title:str,log_axis:str):
-    """
-    Funkcja do tworzenia wykresów z zastosowaniem skali logarytmicznej.  
-    Szczegółowy opis znajduje się w zadaniu 7.
+    if x.size == 0 or y.size == 0 or x.size != y.size:
+        return None
 
-    Parameters
-    ----------
-    x : np.ndarray  
-        Wektor wartości osi X.  
-    y : np.ndarray  
-        Wektor wartości osi Y.  
-    xlabel : str  
-        Etykieta osi X.  
-    ylabel : str  
-        Etykieta osi Y.  
-    title : str  
-        Tytuł wykresu.  
-    log_axis : str  
-        Określa, na której osi zastosować skalę logarytmiczną:  
-        - `'x'`  → logarytmiczna skala osi X,  
-        - `'y'`  → logarytmiczna skala osi Y,  
-        - `'xy'` → logarytmiczna skala na obu osiach.  
+    if log_axis not in ['x', 'y', 'xy']:
+        return None
 
-    Returns
-    -------
-    matplotlib.pyplot.figure  
-        Wykres (x, y) ze skalą logarytmiczną, zgodny z opisem z zadania 7.  
-    """
-    return None
+    mask = (x > 0) & (y > 0) if log_axis == 'xy' else \
+           (x > 0) if log_axis == 'x' else \
+           (y > 0) if log_axis == 'y' else np.ones_like(x, dtype=bool)
 
+    x = x[mask]
+    y = y[mask]
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+
+    if log_axis == 'x':
+        ax.set_xscale('log')
+    elif log_axis == 'y':
+        ax.set_yscale('log')
+    elif log_axis == 'xy':
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+
+    plt.show()
+    return fig
